40cbe2c1125b740a54df2cb46b032834f4e876b6
Mark Vayngrib <mark.vayngrib@lablz.com>
2015-08-03 12:24:09 -0400

    stop using zuul browser tests for now

--- test/node/http-browserify.js
+++ test/node/http-browserify.js
@@ -4,18 +4,8 @@ var test = require('tape')
 
 var parseUrl = require('url').parse
 global.fetch = require('node-fetch')
-// window.location = {
-// 		hostname: 'localhost',
-// 		port: 8081,
-// 		protocol: 'http:'
-// }
 
 var noop = function() {}
-// window.XMLHttpRequest = function() {
-// 	this.open = noop
-// 	this.send = noop
-// 	this.withCredentials = false
-// }
 
 var moduleName = require.resolve('../../')
 delete require.cache[moduleName]
@@ -70,48 +60,4 @@ test('Test alt protocol', function(t) {
   t.equal( request._url, 'foo://localhost:3000/bar', 'Url should be correct');
   t.end();
 
-});
-
-test('Test withCredentials param', function(t) {
-  var url = 'http://localhost/api/foo'
-  var parsed = parseUrl('http://localhost/api/foo');
-  parsed.withCredentials = false
-
-  var request = http.request(parsed, noop);
-  t.equal( request.xhr.withCredentials, false, 'xhr.withCredentials should be false');
-
-  parsed.withCredentials = true
-  var request = http.request(parsed, noop);
-  t.equal( request.xhr.withCredentials, true, 'xhr.withCredentials should be true');
-
-  var request = http.request(url, noop);
-  t.equal( request.xhr.withCredentials, true, 'xhr.withCredentials should be true');
-
-  t.end();
-});
-
-test('Test POST XHR2 types', function(t) {
-  t.plan(3);
-  var url = 'http://localhost/api/foo';
-  var parsed = parseUrl(url)
-  parsed.method = 'POST'
-
-  var request = http.request(url, noop);
-  request.xhr.send = function (data) {
-    t.ok(data instanceof global.ArrayBuffer, 'data should be instanceof ArrayBuffer');
-  };
-
-  request.end(new global.ArrayBuffer());
-
-  request = http.request(parsed, noop);
-  request.xhr.send = function (data) {
-    t.ok(data instanceof global.Blob, 'data should be instanceof Blob');
-  };
-  request.end(new global.Blob());
-
-  request = http.request(parsed, noop);
-  request.xhr.send = function (data) {
-    t.ok(data instanceof global.FormData, 'data should be instanceof FormData');
-  };
-  request.end(new global.FormData());
 });
